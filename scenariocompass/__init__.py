45a958b5b3528214f8e77254106247ca6efb1346
Daniel Huppmann <dh@dergelbesalon.at>
2026-06-27 12:25:32 +0200

    Add same logging as pyam, ixmp4, nomenclature

--- scenariocompass/__init__.py
+++ scenariocompass/__init__.py
@@ -1,2 +1,24 @@
+import logging
+import sys
+from pathlib import Path
+
+import yaml
+
 from .emissions_diagnostics import EmissionsDiagnostics  # noqa
 from .historical_vetting import HistoricalVetting  # noqa
+
+here = Path(__file__).parent
+
+try:
+    __IPYTHON__  # type: ignore
+    _in_ipython_session = True
+except NameError:
+    _in_ipython_session = False
+
+_sys_has_ps1 = hasattr(sys, "ps1")
+
+# Logging is only configured by default when used in an interactive environment.
+# This follows the setup in ixmp4, pyam and nomenclature.
+if _in_ipython_session or _sys_has_ps1:
+    with open(here / "logging.yaml") as file:
+        logging.config.dictConfig(yaml.safe_load(file))
