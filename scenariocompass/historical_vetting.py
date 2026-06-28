58b3928ee52f1bc35cfb83aa47dcb062c3687876
Daniel Huppmann <dh@dergelbesalon.at>
2026-06-28 07:41:32 +0200

    Rework section for required variables

--- scenariocompass/historical_vetting.py
+++ scenariocompass/historical_vetting.py
@@ -4,6 +4,8 @@ from pathlib import Path
 from nomenclature.processor import Processor, DataValidator
 from nomenclature.processor.data_validator import WarningEnum
 from pyam import IamDataFrame
+from pyam.exceptions import format_log_message
+from pyam.utils import format_n, make_index
 
 here = Path(__file__).absolute().parent
 criteria_dir = here.parent / "criteria" / "validate_data"
@@ -13,7 +15,7 @@ logger = logging.getLogger(__name__)
 
 
 class HistoricalVetting(Processor):
-    prefix : str = "Historical Vetting"
+    prefix: str = "Historical Vetting"
     validators: list[DataValidator] = [
         DataValidator.from_file(criteria_dir / "historical_emissions.yaml"),
         DataValidator.from_file(criteria_dir / "historical_energy_balances.yaml"),
@@ -27,9 +29,7 @@ class HistoricalVetting(Processor):
         """Reset validator-item-names to "Historical Vetting|<Variable>|<Year>" """
         for validator in self.validators:
             for item in validator.criteria_items:
-                item.name = "|".join(
-                    [self.prefix, item.variable[0], str(item.year[0])]
-                )
+                item.name = "|".join([self.prefix, item.variable[0], str(item.year[0])])
 
     @property
     def criteria_names(self):
@@ -45,25 +45,38 @@ class HistoricalVetting(Processor):
         self._update_names()
         df = self.reset_apply(df)
 
-        # use `exclude` attribute to track missing data
-        previous_exclude = df.exclude.copy()
-        df.exclude = False
+        # assume that all scenarios passed the vetting
+        df.set_meta(name="Vetting|SCI 2025", meta="passed")
 
-        # change error to warning
+        # check that required variables exist
+        required_variable_list = []
+        for validator in self.validators:
+            required_variable_list.extend(validator.input_data["variable"])
+        missing_data = df.require_data(
+            variable=required_variable_list, year=[2020, 2025],
+        )
+        if missing_data is not None:
+            logger.warning(
+                format_log_message(
+                    "The following data are missing to do historical vetting",
+                    missing_data,
+                )
+            )
+            df.set_meta(
+                name="Vetting|SCI 2025",
+                meta="insufficient reporting",
+                index=make_index(missing_data, ["model", "scenario"]),
+            )
+
+        # change error to warning and run validation
+        # TODO consider custom log messages
         for validator in self.validators:
             _validator = validator.model_copy()
             for item in _validator.criteria_items:
-                df.require_data(**item.filter_args, exclude_on_fail=True)
                 item.validation[0].warning_level = WarningEnum(40)
-
             df = _validator.apply(df)
 
-        df.set_meta(name="Vetting|SCI 2025", meta="passed")
-        df.set_meta(
-            name="Vetting|SCI 2025",
-            meta="insufficient reporting",
-            index=df.filter(exclude=True).index,
-        )
+        # assign aggregate meta-indicator from all validation criteria items
         for col in df.meta.columns:
             if col.startswith(self.prefix):
                 df.meta[col] = df.meta[col].replace({"high": "failed"})
@@ -73,14 +86,13 @@ class HistoricalVetting(Processor):
         ]
 
         failed_vetting = vetting_result.apply(
-            lambda x: any([i == "failed" for i in x]),
-            axis=1,
+            lambda x: any([i == "failed" for i in x]), axis=1
         )
         df.set_meta(
-            name="Vetting|SCI 2025", meta="failed", index=failed_vetting[failed_vetting].index
+            name="Vetting|SCI 2025",
+            meta="failed",
+            index=failed_vetting[failed_vetting].index,
         )
-
-        df._exclude = previous_exclude
 
         return df
 
