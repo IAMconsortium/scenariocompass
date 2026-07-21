ff739aca2fa64f5029a904b9346d45857959f167
Daniel Huppmann <dh@dergelbesalon.at>
2026-07-21 19:52:24 +0200

    Add indicator for net-negative cumulative emissions (#10)

--- scenariocompass/emissions_diagnostics.py
+++ scenariocompass/emissions_diagnostics.py
@@ -1,9 +1,14 @@
-from nomenclature.processor import Processor
+import logging
 
+from nomenclature.processor import Processor
 import pyam
+from pyam import IamDataFrame
+
+logger = logging.getLogger(__name__)
 
 
 class EmissionsDiagnostics(Processor):
+    prefix: str = "Emissions Diagnostics"
     input_data: dict[str, list[str]] = dict(
         variable=[
             "Emissions|CO2",
@@ -16,11 +21,14 @@ class EmissionsDiagnostics(Processor):
         "Emissions Diagnostics|Cumulative CO2 [2020-2100, Gt CO2]",
         "Emissions Diagnostics|Cumulative Kyoto Gases [2020-2100, Gt CO2e]",
         "Emissions Diagnostics|Cumulative CCS [2020-2100, Gt CO2]",
+        "Emissions Diagnostics|Cumulative Net-Negative CO2 [2020-2100, Gt CO2]"
         "Emissions Diagnostics|Year of Net Zero|Kyoto Gases",
         "Emissions Diagnostics|Year of Net Zero|CO2",
     ]
 
     def apply(self, df: pyam.IamDataFrame):
+
+        df = self.reset_apply(df)
 
         _df = df.filter(**self.input_data, keep=True, inplace=False)
         if _df.empty:
@@ -40,32 +48,65 @@ class EmissionsDiagnostics(Processor):
         }.items():
             df.set_meta(
                 name="Emissions Diagnostics|" + name,
-                meta=compute_cumulative_eoc(_df.filter(variable=variable)),
+                meta=(
+                    _df.filter(variable=variable)
+                    .timeseries()
+                    .apply(
+                        lambda x: pyam.timeseries.cumulative(x, 2020, 2100),
+                        raw=False,
+                        axis=1,
+                    )
+                    / 1000
+                ),
             )
 
-        # TODO Emissions Diagnostics|Cumulative Net-Negative CO2 [2020-2100, Gt CO2]",
+        df.set_meta(
+            name=f"{self.prefix}|Cumulative Net-Negative CO2 [2020-2100, Gt CO2]",
+            meta=(
+                _df.filter(variable="Emissions|CO2")
+                .timeseries()
+                .apply(compute_cumulative_net_negative_emissions, raw=False, axis=1)
+                / 1000
+            ),
+        )
 
         for species in ["Kyoto Gases", "CO2"]:
             df.set_meta(
-                name=f"Emissions Diagnostics|Year of Net Zero|{species}",
-                meta=_df.filter(variable=f"Emissions|{species}")
-                .timeseries()
-                .apply(year_of_netzero, raw=False, axis=1),
+                name=f"{self.prefix}|Year of Net Zero|{species}",
+                meta=(
+                    _df.filter(variable=f"Emissions|{species}")
+                    .timeseries()
+                    .apply(year_of_netzero, raw=False, axis=1)
+                ),
             )
 
         return df
 
+    def reset_apply(self, df: IamDataFrame) -> IamDataFrame:
 
-def compute_cumulative_eoc(df):
-    if df.empty:
+        cols = [col for col in df.meta.columns if col.startswith(self.prefix + "|")]
+        if cols:
+            logger.info(f"Resetting {len(cols)} '{self.prefix}' meta-indidators")
+            df.meta.drop(cols, axis=1, inplace=True)
+
+        return df
+
+
+def compute_cumulative_net_negative_emissions(x):
+    if 2100 not in x.index:
         return None
 
-    return (
-        df.timeseries().apply(
-            lambda x: pyam.timeseries.cumulative(x, 2020, 2100), raw=False, axis=1
-        )
-        / 1000
-    )
+    return _compute_cumulative(x, 0, pyam.timeseries.cross_threshold(x))
+
+
+def _compute_cumulative(x, value, zero_years):
+    if len(zero_years) == 0:
+        return value
+    elif len(zero_years) == 1:
+        return value + pyam.timeseries.cumulative(x, zero_years[0], 2100)
+    else:
+        value += pyam.timeseries.cumulative(x, zero_years[0], zero_years[1])
+        return _compute_cumulative(x, value, zero_years[2:])
 
 
 def year_of_netzero(x):
--- tests/test_emission_diagnostics.py
+++ tests/test_emission_diagnostics.py
@@ -1,11 +1,16 @@
 from pathlib import Path
 
 import numpy as np
+import numpy.testing as npt
 import pandas as pd
+import pandas.testing as pdt
 import pytest
 
 import pyam
 from scenariocompass import EmissionsDiagnostics
+from scenariocompass.emissions_diagnostics import (
+    compute_cumulative_net_negative_emissions,
+)
 
 here = Path(__file__).parent.absolute()
 
@@ -16,6 +21,7 @@ EXP_COLS = [
     "Emissions Diagnostics|Cumulative CO2 [2020-2100, Gt CO2]",
     "Emissions Diagnostics|Cumulative Kyoto Gases [2020-2100, Gt CO2e]",
     "Emissions Diagnostics|Cumulative CCS [2020-2100, Gt CO2]",
+    "Emissions Diagnostics|Cumulative Net-Negative CO2 [2020-2100, Gt CO2]",
     "Emissions Diagnostics|Year of Net Zero|Kyoto Gases",
     "Emissions Diagnostics|Year of Net Zero|CO2",
 ]
@@ -23,8 +29,8 @@ EXP_COLS = [
 
 EXP_META = pd.DataFrame(
     [
-        ["MESSAGEix 1.1", "ENGAGE-NoPolicy", 5125.85, 6445.72, 0.0, np.nan, np.nan],
-        ["REMIND 3.5", "Rescuing-1.5°C", 211.85, 841.31, 473.23, 2060.0, 2050.0],
+        ["MESSAGEix 1.1", "ENGAGE-NoPolicy", 5125.85, 6445.72, 0.0, 0, np.nan, np.nan],
+        ["REMIND 3.5", "Rescuing-1.5°C", 211.85, 841.31, 473.23, -338.05, 2060, 2050],
     ],
     columns=EXP_COLS,
 ).set_index(["model", "scenario"])
@@ -50,8 +56,8 @@ def test_emissions_diagnostics_short_horizon():
 
     exp = EXP_META.copy()
     exp.iloc[0] = np.nan
-    exp.iloc[1, 0:3] = np.nan
-    pd.testing.assert_frame_equal(df.meta, exp)
+    exp.iloc[1, 0:4] = np.nan
+    pdt.assert_frame_equal(df.meta, exp)
 
 
 def test_emissions_diagnostics_no_global_data():
@@ -61,7 +67,7 @@ def test_emissions_diagnostics_no_global_data():
     df = emission_diagnostics.apply(df)
 
     # fast pass, no meta columns added as part of the processing
-    pd.testing.assert_frame_equal(df.meta, TEST_DF.filter(model="MESSAGEix 1.1").meta)
+    pdt.assert_frame_equal(df.meta, TEST_DF.filter(model="MESSAGEix 1.1").meta)
 
 
 def test_emissions_diagnostics_unknown_unit():
@@ -71,3 +77,22 @@ def test_emissions_diagnostics_unknown_unit():
     match = "Invalid units for emissions diagnostics: foo"
     with pytest.raises(ValueError, match=match):
         emission_diagnostics.apply(df)
+
+
+@pytest.mark.parametrize(
+    "data, value",
+    [
+        # crossing threshold once/twice/three/four times
+        ([100, -10, -20, -10, -30], -1024.5),
+        ([100, -10, -20, -10, 10], -658.5),
+        ([100, -10, 10, -10, -30], -523.5),
+        ([100, -10, 10, -10, 10], -157.5),
+        # reaching zero from below / from above but not crossing
+        ([100, -10, 0, -10, 10], -259.0),
+        ([100, -10, 10, 0, 10], -58.5),
+    ],
+)
+def test_net_negative_emissions(data, value):
+
+    x = pd.Series(data, index=[2020, 2040, 2060, 2080, 2100])
+    npt.assert_almost_equal(compute_cumulative_net_negative_emissions(x), value)
